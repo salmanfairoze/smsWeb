15be1448c95096e60625d1a02992310a7e11a7fd
pranav <aadityapranav1@gmail.com>
2020-06-10 17:44:36 +0530

    added covid stats changes

---
+++ covid_stats.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup as bs
+from urllib.request import urlopen
+import pandas as pd
+
+url="https://www.mohfw.gov.in"
+query = "Tamil Nadu"
+soup = bs(urlopen(url),'lxml')
+for table in soup.findAll('tbody'):
+	row=table.findAll('tr')
+	break;
+sno=[]
+name=[]
+active=[]
+cured_dis_mig=[]
+deaths=[]
+total_confirmed=[]
+for col in row :
+	c=col.findAll('td')
+	if(len(c)==6):
+		sno.append(c[0].text)
+		name.append(c[1].text.lower())
+		active.append(c[2].text)
+		cured_dis_mig.append(c[3].text)
+		deaths.append(c[4].text)
+		total_confirmed.append(c[5].text)	
+		
+#print(name)
+df = pd.DataFrame({'sno':sno,'Name':name,"active":active,'Cured':cured_dis_mig,'deaths':deaths,"total_confirmed":total_confirmed}) 
+print(df.loc[df['Name'] == query.lower()])
+df.to_csv('cases.csv', index=False, encoding='utf-8')	
--- htmltotxt_python3.py
+++ htmltotxt_python3.py
@@ -1,12 +1,12 @@
-import urllib2
-from urllib2 import urlopen
-from urllib2 import Request
+import urllib.request as urllib2
+from urllib.request import urlopen
+from urllib.request import Request
 import html2text
-query = "Worlds smallest country"
+query = "Worldssmallestcountry"
 url = 'https://www.google.com/search?client=ubuntu&channel=fs&q={}&ie=utf-8&oe=utf-8'.format(query)
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}  
-req = urllib2.Request(url, headers = headers)
-page = urllib2.urlopen(req)
+req = Request(url, headers = headers)
+page =urlopen(req)
 # page = urllib2.urlopen(url)
 html_content = page.read().decode('utf-8')
 h = html2text.HTML2Text()
@@ -15,5 +15,5 @@ h.ignore_emphasis = True
 h.escape_all = True
 rendered_content = h.handle(html_content).encode('utf-8')
 file = open('file_text.txt', 'w')
-file.write(rendered_content)
+file.write(rendered_content.decode('utf-8'))
 file.close()
