e17e440950de5671b6033c77dadd95986310da9d
Durga Prasad <durgaprasadsn123@gmail.com>
2020-06-14 02:14:58 +0530

    Formatted Zones

--- zones.py
+++ zones.py
@@ -9,7 +9,23 @@ from bs4 import BeautifulSoup
 import time
 import json
 
-def calculate_cases(state, district):
+def calculate_cases(query):
+    url = 'https://www.google.com/search?client=ubuntu&channel=fs&q={}&ie=utf-8&oe=utf-8'.format(query)
+    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}  
+    req = Request(url, headers = headers)
+    page =urlopen(req)
+    # page = urllib2.urlopen(url)
+    html_content = page.read().decode('utf-8')
+
+    soup = BeautifulSoup(html_content, 'lxml')
+    div_tag = soup.find("div", attrs={"class":'SPZz6b'})
+    span_tag = div_tag.findAll("span")
+    temp_state = span_tag[1].contents[0].split(" ")[2:]
+
+    state = " ".join(temp_state)
+    district = span_tag[0].contents[0]
+    print(state, district)
+
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get("https://www.covid19india.org/")
     time.sleep(3)
@@ -19,13 +35,10 @@ def calculate_cases(state, district):
 
     row=soup.findAll('div', attrs={'class':'row'})
 
-    # state = "Mizoram"
-    # district = "Aizawl"
-
     for i in range(1,len(row)):
         name=row[i].find('div', attrs={'class':'state-name'})
         if(state in name.text):
-            print(name.text)
+            # print(name.text)
             j=i
             break
 
@@ -43,63 +56,48 @@ def calculate_cases(state, district):
     active_state = tot[1].text
     recovered_state = tot[2].text
     dead_state = tot[3].text
-    print("confirmed:" , confirmed_state, "active: ", active_state, "Recovered: ", recovered_state, "Dead: ", dead_state)
+    # print("confirmed:" , confirmed_state, "active: ", active_state, "Recovered: ", recovered_state, "Dead: ", dead_state)
     row=soup.findAll('div', attrs={'class':'row district'})
     for k in row:
         if(district in k.find('div', attrs={'class':'state-name'}).text):
-            print(k.find('div', attrs={'class':'state-name'}).text)
+            # print(k.find('div', attrs={'class':'state-name'}).text)
             tot=k.findAll('div', attrs={'class':'total'})
             confirmed_district = tot[0].text
             active_district = tot[1].text
             recovered_district = tot[2].text
             dead_district = tot[3].text
-            print("confirmed:", confirmed_district, "active: ", active_district, "Recovered: ", recovered_district, "Dead: ", dead_district)
+            # print("confirmed:", confirmed_district, "active: ", active_district, "Recovered: ", recovered_district, "Dead: ", dead_district)
             active_district = active_district.replace(",", "")
             break
     
     active_state = active_state.replace(",", "")
     if (active_district != -1):
-        percentage_wrt_state = (int(active_district) / int(active_state))
+        percentage_wrt_state = ((int(active_district) / int(active_state)) * 100)
         if (percentage_wrt_state >= 0.8):
-            print("RED ZONE")
+            zone = "RED ZONE"
+            # print("RED ZONE")
         else:
             url = "https://api.covid19india.org/districts_daily.json"
             response = urlopen(url)
             data = json.loads(response.read())
-            print(data['districtsDaily'][state][district][-1])
+            # print(data['districtsDaily'][state][district][-1])
 
             flag = 0
             for i in range(1, 28):
                 if (data['districtsDaily'][state][district][-i]["active"] - data['districtsDaily'][state][district][-i-1]["active"] > 0):
-                    print("Orange Zone ")
+                    zone = "ORANGE ZONE"
+                    # print("Orange Zone ")
                     flag = 1
                     break
             if (flag == 0):
-                print("Green Zone")
-            print(percentage_wrt_state)
+                zone = "GREEN ZONE"
+        # print(zone, active_state, active_district, confirmed_state, confirmed_district, recovered_state, recovered_district)
+        res = zone + '\n' + "District - " + district + '\n\t- ' + str(active_district) + " - Active Cases" + "\n\t- " + str(confirmed_district) + " - Confirmed Cases" + "\n\t- " + str(recovered_district) + " - Recovered Cases" + "\n" + str(percentage_wrt_state) + " Percentage of active cases wrt State" + "\n" + "State - " + state + "\n\t- " + str(active_state) + " - Active Cases" + "\n\t- " + str(confirmed_state) + " - Confirmed Cases" + "\n\t- " + str(recovered_state) + " - Recovered Cases"
+        print(res)
+        return res 
     else:
         print("It is not a District")
+        return 404
 
-def get_zone(query):
-    url = 'https://www.google.com/search?client=ubuntu&channel=fs&q={}&ie=utf-8&oe=utf-8'.format(query)
-    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}  
-    req = Request(url, headers = headers)
-    page =urlopen(req)
-    # page = urllib2.urlopen(url)
-    html_content = page.read().decode('utf-8')
-
-    soup = BeautifulSoup(html_content, 'lxml')
-    div_tag = soup.find("div", attrs={"class":'SPZz6b'})
-    span_tag = div_tag.findAll("span")
-    temp_state = span_tag[1].contents[0].split(" ")[2:]
-
-    state = " ".join(temp_state)
-    district = span_tag[0].contents[0]
-    print(state, district)
-    calculate_cases(state, district)
-
-
-# Input
-query = "ramanagara"
-
-get_zone(query)
+district = "Kurnool"
+result = calculate_cases(district)
